a6a99ec4a6c7515673d1674013a2a3d830a54dff
GoodmanDev <david.g.goodman@bath.edu>
2021-05-22 14:49:58 +0100

    Update java-web-project to make better use of image layer caching

--- java-web-project/src/main/java/dev/goodman/web/controller/WelcomeController.java
+++ java-web-project/src/main/java/dev/goodman/web/controller/WelcomeController.java
@@ -15,7 +15,7 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        logger.debug("Welcome! :)");
+        logger.debug("Welcome!! :D");
         return "index";
     }
 
